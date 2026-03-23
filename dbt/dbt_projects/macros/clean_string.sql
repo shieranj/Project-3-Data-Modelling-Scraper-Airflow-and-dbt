754942fe41a2236909a6f8faee338655e445543c
shieranj <shieranjuvi.sj@gmail.com>
2026-03-23 16:06:19 +0700

    gitignore changes & pycache deletion

--- dbt/dbt_projects/macros/clean_string.sql
+++
@@ -1,6 +0,0 @@
-{% macro clean_string(column_name) %}
-    CASE
-        WHEN TRIM({{ column_name }}) IN ("","—") THEN NULL
-        ELSE TRIM({{ column_name }})
-    END
-{% endmacro %}
--- dbt/dbt_projects/models/ecommerce/core/dim_category.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='table',
-) }}
-
-SELECT 
-    DISTINCT category_id,
-    category_name,
-    CURRENT_TIMESTAMP() AS dbt_processed_date
-FROM {{ ref ('prep_products') }}
